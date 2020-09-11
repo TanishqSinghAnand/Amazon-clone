f6352a189c3eef0172f31f5e38b7fa09c21a2a53
Tanishq0504 <codertanihsq@gmail.com>
2020-09-11 15:47:02 +0530

    layout

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDi2qLV_7W7C94i3HfJNOPovHIKmGvpEtA",
+    authDomain: "clone-8c3a2.firebaseapp.com",
+    databaseURL: "https://clone-8c3a2.firebaseio.com",
+    projectId: "clone-8c3a2",
+    storageBucket: "clone-8c3a2.appspot.com",
+    messagingSenderId: "1090922807012",
+    appId: "1:1090922807012:web:ef70e73985e57997a4f6f1",
+    measurementId: "G-CQGZCMYKHN"
+  };
